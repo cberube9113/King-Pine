bc7981610f264ff065f5ff685e1f26a9aadfd8a9
Steven Tenaglia <stenagli@student.umass.edu>
2013-03-11 15:11:42 -0400

    Added Chirps database and made /home print some tweets

--- routes/home.js
+++ routes/home.js
@@ -1,8 +1,11 @@
 var user = require('../lib/user.js');
+var chirps = require('../lib/chirps.js');
 
 exports.list = function(req,res) {
 	var u = user.info('rsims');
+	var c = chirps.info();
     res.render('home', { title: 'Chirper',
     					 name: u.name,
-    					 email: u.email });
+    					 email: u.email,
+    					 chirps: c  });
 };
