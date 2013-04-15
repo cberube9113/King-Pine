864829ff26ec7c239d543d6c8fc9fdb9a0e8a070
rsims <rsims2013@gmail.com>
2013-04-15 12:03:55 -0400

    Start to making the chirp box access the database

--- routes/chirp-socket.js
+++ routes/chirp-socket.js
@@ -1,8 +1,18 @@
+var user = require('../lib/user.js');
+var chirps = require('../lib/chirps.js');
+
 //Server-side support for Chirp app:
 
+addLocalChirp = function(req,res,data){
+	var username = req.session.user.username;
+	var uid = user.idlookup(username);
+	var date = "Just now.";
+	chirps.addChirp(data,date,uid);
+}
 exports.init = function(socket) {
 	socket.on('post', function(data) {
 		console.log('Received post: ' + JSON.stringify(data));
+		addLocalChirp(data);
 	socket.broadcast.emit('post', data);
 	});
 };
