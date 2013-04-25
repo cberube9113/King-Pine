4ccb35aa0eff946483a7e5f135a53baf334d4faa
Steven Tenaglia <stenagli@student.umass.edu>
2013-04-25 19:48:58 -0400

    Home and Discover pages now use SQLite database

--- routes/discover.js
+++ routes/discover.js
@@ -1,15 +1,29 @@
 var chirps = require('../lib/chirps.js');
+var sql = require('../lib/sql.js');
 
 // ##list
 //Renders discover page
 
 exports.list = function(req,res) {
 	if(req.session.user != undefined){ //If req.session.user is a value other than undefined, there is a user logged in.
-		var discoveryChirps = chirps.discoveryChirps();
-  	  	res.render('discover', { title: 'Discover'
+		//var discoveryChirps = chirps.discoveryChirps();
+		
+		sql.homeChirps(function(err,chirpDB){
+
+			if(err){
+		  		 console.log('SQLite error!');
+			}
+	
+	   	else {
+	   		var discoveryChirps = chirps.discoveryChirps(chirpDB);	
+		
+  	  			res.render('discover', { title: 'Discover'
     							, discoveryChirps: discoveryChirps
     							, user: req.session.user.name  });
-    }
+    		}
+		});
+
+	}
     
     else{ //If there isn't a user logged in, redirect to /index with a message.
     	req.flash('auth', 'You need to be logged in to do that!');
--- routes/home.js
+++ routes/home.js
@@ -1,23 +1,40 @@
 var user = require('../lib/user.js');
 var chirps = require('../lib/chirps.js');
+var sql = require('../lib/sql.js');
+var follow = require('../lib/follow.js');
 
 // ##list
 //Renders home page
 
 exports.list = function(req,res) {
 	if(req.session.user != undefined){ //If req.session.user is a value other than undefined, there is a user logged in.
-		var subject = req.session.user;
+	   var subject = req.session.user;
+	   var username = subject.username;
 		var userdata = user.info(subject.username);
-		var chirpdata = chirps.homeChirps(subject.username);
-    	res.render('home', { title: 'Chirper',
+		sql.homeChirps(function(err,chirpDB){
+
+		if(err){
+		   console.log('SQLite error!');
+		}
+	
+	   else {
+	   	var homeChirps = chirps.homeChirps(username,chirpDB);
+		
+	   	res.render('home', { title: 'Chirper',
     						 name: userdata.name,
     						 email: userdata.email,
-    						 chirps: chirpdata,
+    						 chirps: homeChirps,
     						 user: req.session.user.name,
                              userid: req.session.user.id,
                              username: userdata.username,
     						 message: req.flash('auth')
-    	});
+    	   });
+	   }
+		
+		
+		
+		});
+	
     }
     
     else{ //If there isn't a user logged in, redirect to /index with a message.
