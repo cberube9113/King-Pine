050b02bf26d9f9a0414e35462632688509e13706
Steven Tenaglia <stenagli@student.umass.edu>
2013-05-03 01:49:40 -0400

    Posting a chirp, including adding to database and socket.io, are now working

--- routes/chirp-socket.js
+++ routes/chirp-socket.js
@@ -4,14 +4,14 @@ var sql = require('../lib/sql.js');
 
 //Server-side support for Chirp app:
 
-addLocalChirp = function(data, uid, date){
-	sql.addChirp(data, uid, date);
+addLocalChirp = function(data, uid, date, username, name){
+	sql.addChirp(data, uid, date, username, name);
 }
 
 exports.init = function(socket, io) {
 	socket.on('chirp', function(data) {
 		console.log('Received post: ' + JSON.stringify(data));
-		addLocalChirp(data["chirp"], data["userid"], data["timestamp"]);
+		addLocalChirp(data["chirp"], data["userid"], data["timestamp"], data["username"], data["name"]);
 		io.sockets.emit('updateView', data);
 	});
 };
