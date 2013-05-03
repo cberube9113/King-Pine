121f376906bcea165f6a36e010d7b432b2ce6127
Steven Tenaglia <stenagli@student.umass.edu>
2013-05-03 11:46:23 -0400

    Me page now uses databases. Just have to implement the follow/unfollow button to use databases now...

--- routes/me.js
+++ routes/me.js
@@ -1,6 +1,7 @@
 var follow = require('../lib/follow.js');
 var chirps = require('../lib/chirps.js');
 var sql = require('../lib/sql.js');
+var async = require('async');
 
 // ##list
 //Renders me page
@@ -13,11 +14,56 @@ exports.list = function(req,res) {
         locals.user = session.name;
         locals.username = session.username;
 
+        async.parallel([
+            // Get number of people you're following
+            function(callback){
+                sql.getNumFollowing(session.uid, function(err, countObject){
+                    locals.following = countObject.count;
+                    callback();
+                })
+            },
+
+            // Get number of people following you
+            function(callback){
+                sql.getNumFollowers(session.uid, function(err, countObject){
+                    locals.followers = countObject.count;
+                    callback();
+                });
+            },
+
+            // Get number of chirps
+            function(callback){
+                sql.getNumChirps(session.uid, function(err, countObject){
+                    locals.nchirps = countObject.count;
+                    callback();
+                })
+            },
+
+            // Get array of chirps
+            function(callback){
+                sql.meChirps(session.uid, function(err, arrayOfChirps){
+                    locals.chirpdata = arrayOfChirps;
+                    callback();
+                })
+            }],
+
+            // Final callback after all parallel calls
+            function(err){
+                if(err){
+                    console.log('ERROR!');
+                }
+                else{
+                    res.render('me', locals);
+                }
+            }
+
+            );
+
         
 
 
 
-
+/*
         var subject = req.session.user;
 		var following = follow.numfollowing(subject.username);
 		var followers = follow.numfollowers(subject.username);
@@ -31,6 +77,7 @@ exports.list = function(req,res) {
     				   user: req.session.user.name,
                        username: req.session.user.username
                     });
+*/
     }
     
     else{ //If there isn't a user logged in, redirect to /index with a message.
